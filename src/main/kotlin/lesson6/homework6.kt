8038f14d8d82124aa817fa980ac8084cb092c58b
Isaev_Evgeniy <evgenij_isaev_91@list.ru>
2024-09-22 20:08:04 +0400

    commit_6

--- src/main/kotlin/lesson6/homework6.kt
+++ src/main/kotlin/lesson6/homework6.kt
@@ -1,18 +1,37 @@
 package com.isaev.lesson6
 
 fun main () {
-    seasonDefinition(15)
-
+    println(seasonDefinition(5))
+    println(ageOfThePet(18))
+    println(transport(6))
 }
 
-fun seasonDefinition (month: Int?) {
-    return if (month in 1..12) {
-        when (month) {
-            1, 2, 12 -> println("Winter")
-            3, 4, 5 -> println("Spring")
-            6, 7, 8 -> println("Summer")
-            9, 10, 11 -> println("Autumn")
-            else -> println("Invalid format")
-        }
-    } else {println("Invalid format")}
+fun seasonDefinition (month: Int?) : Any {
+    return if (month in 1..2 || month == 12) {
+        "Winter"
+    } else if (month in 3..5) {
+        "Spring"
+    } else if (month in 6..8) {
+        "Summer"
+    } else if (month in 8..11) {
+        "Autumn"
+    } else {
+        "Invalid format"
+    }
 }
+
+fun ageOfThePet(age: Int): Number {
+    return if (age <= 2) {
+        age * 10.5
+    } else {
+        (2 * 10.5) + ((age - 2) * 4)
+    }
+}
+
+fun transport (distance: Int): Any {
+    return  if (distance in 0 .. 1) {"on foot"}
+            else if (distance in 2 .. 5) {"bicycle"}
+            else {"motor transport"}
+    }
+
+
